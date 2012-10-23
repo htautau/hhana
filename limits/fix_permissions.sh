8b4d08c8ffae987cccb65b460618840a2dae6e89
Noel Dawe <noel.dawe@gmail.com>
2012-10-23 12:28:33 -0700

    ignore permissions errors in chmod

--- limits/fix_permissions.sh
+++ limits/fix_permissions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find -H data results -type d | xargs chmod 770
-find -H data results -type f | xargs chmod 660
+find -H data results -type d | xargs chmod -f 770
+find -H data results -type f | xargs chmod -f 660
