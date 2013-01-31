b790494329e1bc61f7e015f0b82e0e014a50bf86
Noel Dawe <noel.dawe@gmail.com>
2013-01-31 15:21:19 +0100

    add histfactory modules

---
+++ mva/limits/histfactory.py
@@ -0,0 +1,57 @@
+import ROOT
+
+
+def make_channel(name, data=None):
+
+    chan = ROOT.RooStats.HistFactory.Channel(name)
+    if data is not None:
+        # data is a TF1
+        chan.SetData(data)
+    chan.SetStatErrorConfig(0.05, "Poisson")
+
+    # Create the signal sample
+    signal = ROOT.RooStats.HistFactory.Sample("signal", "signal", InputFile)
+    signal.AddOverallSys("syst1",  0.95, 1.05)
+    signal.AddNormFactor("SigXsecOverSM", 1, 0, 3)
+    chan.AddSample(signal)
+
+    # Background 1
+    background1 = ROOT.RooStats.HistFactory.Sample("background1", "background1", InputFile)
+    background1.ActivateStatError("background1_statUncert", InputFile)
+    background1.AddOverallSys("syst2", 0.95, 1.05 )
+    chan.AddSample(background1)
+
+    # Background 1
+    background2 = ROOT.RooStats.HistFactory.Sample("background2", "background2", InputFile)
+    background2.ActivateStatError()
+    background2.AddOverallSys("syst3", 0.95, 1.05 )
+    chan.AddSample(background2)
+
+    return chan
+
+
+def make_measurement(name, title='',
+                     channels,
+                     lumi=1.0, lumi_rel_err=0.,
+                     output_prefix='./results',
+                     POI='SigXsecOverSM'):
+    """
+    Note: to use the workspace in-memory use::
+         static RooWorkspace* MakeCombinedModel(Measurement& measurement);
+    """
+
+    # Create the measurement
+    meas = ROOT.RooStats.HistFactory.Measurement(name, title)
+
+    meas.SetOutputFilePrefix(output_prefix)
+    meas.SetPOI(POI)
+    meas.AddConstantParam("Lumi")
+
+    meas.SetLumi(lumi)
+    meas.SetLumiRelErr(lumi_rel_error)
+    meas.SetExportOnly(False)
+
+    for channel in channels:
+        meas.AddChannel(channel)
+
+    return meas
