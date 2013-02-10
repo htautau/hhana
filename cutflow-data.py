444d8960b005f303abd1a8273ee24239fc4111bd
Noel Dawe <noel.dawe@gmail.com>
2013-02-10 13:35:43 -0800

    mv cutflow scripts here

--- cutflow-data.py
+++ cutflow-data.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 
-from higgstautau.cutflow import get_parser, make_cutflow
+from mva.cutflow import get_parser, make_cutflow
 
 
 parser = get_parser()
 parser.add_argument('--dir', default='../ntuples/midpt')
+parser.add_argument('--year', default=2012)
 args = parser.parse_args()
+
+
+from pyAMI.client import AMIClient
+from pyAMI.query import get_periods
+
+
+periods = get_periods(AMIClient(), year=args.year)
+
+print periods
+
+import sys
+sys.exit(1)
+
+
 
 samples = [
     ('B', 'B', 'data-B'),
