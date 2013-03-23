3c11f34e1f4f752a79a5e4b7ac1cf4848f4075e6
Noel Dawe <Noel.Dawe@cern.ch>
2013-03-22 23:03:21 -0700

    enable trigger efficiency corrections

--- mva/systematics.py
+++ mva/systematics.py
@@ -27,7 +27,7 @@ SYSTEMATICS = {
     'TES': (('TES_UP',), ('TES_DOWN',)),
     #'MFS': (('MFS_UP',), ('MFS_DOWN',)),
     #'ISOL': (('ISOL_UP',), ('ISOL_DOWN',)),
-    #'TRIGGER': (('TRIGGER_UP',), ('TRIGGER_DOWN',)),
+    'TRIGGER': (('TRIGGER_UP',), ('TRIGGER_DOWN',)),
     #'FAKERATE': (('FAKERATE_UP',), ('FAKERATE_DOWN',)),
     'TAUID': (('TAUID_UP',), ('TAUID_DOWN',)),
     'QCD_FIT': (('QCDFIT_UP',), ('QCDFIT_DOWN',)),
@@ -35,16 +35,16 @@ SYSTEMATICS = {
 }
 
 WEIGHT_SYSTEMATICS = {
-    #'TRIGGER': {
-    #    'UP': [
-    #        'tau1_trigger_scale_factor_high',
-    #        'tau2_trigger_scale_factor_high'],
-    #    'DOWN': [
-    #        'tau1_trigger_scale_factor_low',
-    #        'tau2_trigger_scale_factor_low'],
-    #    'NOMINAL': [
-    #        'tau1_trigger_scale_factor',
-    #        'tau2_trigger_scale_factor']},
+    'TRIGGER': {
+        'UP': [
+            'tau1_trigger_scale_factor_high',
+            'tau2_trigger_scale_factor_high'],
+        'DOWN': [
+            'tau1_trigger_scale_factor_low',
+            'tau2_trigger_scale_factor_low'],
+        'NOMINAL': [
+            'tau1_trigger_scale_factor',
+            'tau2_trigger_scale_factor']},
     #'FAKERATE': {
     #    'UP': [
     #        'tau1_fakerate_scale_factor_high',
