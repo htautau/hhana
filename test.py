980b532d948b70bbbbd532282d77d9a4043140cb
Noel Dawe <Noel.Dawe@cern.ch>
2013-03-26 17:28:48 -0700

    updates

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-from mva.samples import Data, MC_Ztautau
+from mva.analysis import Analysis
 from mva.categories import Category_VBF
 
 #a = Data(2012)
 #print a.records(Category_VBF, 'OS', ['tau1_pt'])
 #print a.partitioned_records(Category_VBF, 'OS', ['tau1_pt'], num_partitions=3)
 
-z = MC_Ztautau(2012, systematics=True)
+a = Analysis(2012, Category_VBF, systematics=True)
+
+z = a.ztautau
 
 nom = z.merged_records(Category_VBF, 'OS', ['tau1_pt'], include_weight=False)
 up = z.merged_records(Category_VBF, 'OS', ['tau1_pt'], include_weight=False,
@@ -42,6 +44,16 @@ up = z.merged_records(Category_VBF, 'OS', ['tau1_pt'],
         systematic=('TRIGGER_UP',))
 down = z.merged_records(Category_VBF, 'OS', ['tau1_pt'],
         systematic=('TRIGGER_DOWN',))
+
+print nom['weight'].mean()
+print up['weight'].mean()
+print down['weight'].mean()
+
+nom = z.merged_records(Category_VBF, 'OS', ['tau1_pt'])
+up = z.merged_records(Category_VBF, 'OS', ['tau1_pt'],
+        systematic=('ZFIT_UP',))
+down = z.merged_records(Category_VBF, 'OS', ['tau1_pt'],
+        systematic=('ZFIT_DOWN',))
 
 print nom['weight'].mean()
 print up['weight'].mean()
