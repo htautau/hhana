904e794db01f96f24cf54e9c01812063946704ce
Noel Dawe <Noel.Dawe@cern.ch>
2013-03-28 10:49:31 -0700

    updates

--- mva/categories.py
+++ mva/categories.py
@@ -247,7 +247,7 @@ class Category_Boosted(Category):
     features = features_boosted
     # train with all modes
 
-    halfblind_bins = 5
+    halfblind_bins = 3
 
 
 class Category_Nonboosted_1J(Category):
