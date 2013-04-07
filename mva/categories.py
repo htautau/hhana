69c6f6d7b06a74b0fb90a29cb940815a61d336d3
Noel Dawe <Noel.Dawe@cern.ch>
2013-04-06 18:24:56 -0700

    add mass back in

--- mva/categories.py
+++ mva/categories.py
@@ -67,7 +67,7 @@ CUTS_BOOSTED = Cut('mmc_resonance_pt > 100') # GeV
 
 
 features_2j = [
-    #'mass_mmc_tau1_tau2',
+    'mass_mmc_tau1_tau2',
     # !!! mass ditau + leading jet?
     'dEta_jets',
     #'dEta_jets_boosted', #
@@ -97,7 +97,7 @@ features_2j = [
 ]
 
 features_boosted = [
-    #'mass_mmc_tau1_tau2',
+    'mass_mmc_tau1_tau2',
     # !!! mass ditau + leading jet?
     'dEta_jets',
     #'dEta_jets_boosted', #
@@ -127,7 +127,7 @@ features_boosted = [
 ]
 
 features_1j = [
-    #'mass_mmc_tau1_tau2',
+    'mass_mmc_tau1_tau2',
     # !!! mass ditau + leading jet?
     'sphericity',
     #'sphericity_boosted',
@@ -147,7 +147,7 @@ features_1j = [
 ]
 
 features_0j = [
-    #'mass_mmc_tau1_tau2',
+    'mass_mmc_tau1_tau2',
     #'cos_theta_tau1_tau2',
     'dR_tau1_tau2',
     #'tau1_BDTJetScore',
