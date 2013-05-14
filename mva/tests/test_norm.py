3eea12a26addb74198ab044163e620b76fe1a19a
Noel Dawe <Noel.Dawe@cern.ch>
2013-05-14 03:43:49 -0700

    add unit test that QCD nOS has the same number of expected events as SS (it passes)

---
+++ mva/tests/test_norm.py
@@ -0,0 +1,27 @@
+from mva.analysis import Analysis
+from mva.categories import Category_Cuts_Preselection
+from nose.tools import assert_almost_equal
+
+def test_norm():
+
+    ss = Analysis(
+        year=2012,
+        systematics=False,
+        use_embedding=True,
+        qcd_shape_region='SS_TRK')
+
+    nos = Analysis(
+        year=2012,
+        systematics=False,
+        use_embedding=True,
+        qcd_shape_region='nOS')
+
+    ss.normalize(Category_Cuts_Preselection, 'TRACK')
+    nos.normalize(Category_Cuts_Preselection, 'TRACK')
+
+    qcd_ss = ss.qcd.events(Category_Cuts_Preselection, 'OS_TRK')
+    qcd_nos = nos.qcd.events(Category_Cuts_Preselection, 'OS_TRK')
+
+    # check that nOS and SS QCD models have the same number of events
+    # up to two places after the decimal
+    assert_almost_equal(qcd_ss, qcd_nos, places=2)
