7dc3285b15c41bc06f72b7f993b8fc78005359f1
Noel Dawe <Noel.Dawe@cern.ch>
2013-12-16 20:38:58 -0800

    updates

--- mva/stats/src/runSig.C
+++ mva/stats/src/runSig.C
@@ -63,7 +63,7 @@ TH1D* runSig(RooWorkspace* ws,
     bool doUncap            = 1; // uncap p0
     bool doInj              = 0; // setup the poi for injection study (zero is faster if you're not)
     bool doMedian           = 1; // compute median significance
-    bool isBlind            = 1; // Dont look at observed data
+    bool isBlind            = 0; // Dont look at observed data
     bool doConditional      = !isBlind; // do conditional expected data
     bool doObs              = !isBlind; // compute observed significance
 
