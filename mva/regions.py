df69f5ad5f6041cc845ed6a2aff3f7a754336dd2
Noel Dawe <Noel.Dawe@cern.ch>
2014-01-27 23:47:17 -0800

    update selections

--- mva/regions.py
+++ mva/regions.py
@@ -1,7 +1,7 @@
 from rootpy.tree import Cut
 
-TAUS_PASS = Cut('taus_pass')
-TAUS_FAIL = Cut('!taus_pass')
+#TAUS_PASS = Cut('taus_pass')
+#TAUS_FAIL = Cut('!taus_pass')
 OS = Cut('(tau1_charge * tau2_charge) == -1')
 NOT_OS = Cut('(tau1_charge * tau2_charge) != -1')
 SS = Cut('(tau1_charge * tau2_charge) == 1')
@@ -18,16 +18,20 @@ P1P3_RECOUNTED = (
 
 REGIONS = {
     'ALL': Cut(),
-    'OS': OS & TAUS_PASS,
-    'OSFF': OS & TAUS_FAIL,
-    'OS_TRK': OS & TAUS_PASS & P1P3_RECOUNTED,
-    'OSFF_TRK': OS & TAUS_FAIL & P1P3_RECOUNTED,
-    'nOS': NOT_OS & TAUS_PASS,
-    'nOSFF': NOT_OS & TAUS_FAIL,
-    'SS': SS & TAUS_PASS,
-    'SSFF': SS & TAUS_FAIL,
-    'SS_TRK': SS & TAUS_PASS & P1P3_RECOUNTED,
-    'SSFF_TRK': SS & TAUS_FAIL & P1P3_RECOUNTED}
+    'OS': OS, # & TAUS_PASS,
+    #'OSFF': OS & TAUS_FAIL,
+    'OS_TRK': OS & P1P3_RECOUNTED, # & TAUS_PASS,
+    #'OSFF_TRK': OS & TAUS_FAIL & P1P3_RECOUNTED,
+    'nOS': NOT_OS, # & TAUS_PASS,
+    #'nOSFF': NOT_OS & TAUS_FAIL,
+    'SS': SS, #& TAUS_PASS,
+    #'SSFF': SS & TAUS_FAIL,
+    'SS_TRK': SS & P1P3_RECOUNTED, #& TAUS_PASS
+    #'SSFF_TRK': SS & TAUS_FAIL & P1P3_RECOUNTED
+}
 
 TARGET_REGIONS = ['OS', 'OS_TRK']
-QCD_SHAPE_REGIONS = ['nOS', 'SS', 'SS_TRK', 'OSFF', 'SSFF', 'SSFF_TRK']
+QCD_SHAPE_REGIONS = [
+    'nOS', 'SS', 'SS_TRK',
+    #'OSFF', 'SSFF', 'SSFF_TRK'
+]
