6f4d717c6fe5b0f54be83b3f6804706756075cb3
Noel Dawe <Noel.Dawe@cern.ch>
2014-03-13 00:06:12 -0700

    fix track isolation requirement in mva/regions.py

--- mva/regions.py
+++ mva/regions.py
@@ -1,37 +1,32 @@
 from rootpy.tree import Cut
 
-#TAUS_PASS = Cut('taus_pass')
-#TAUS_FAIL = Cut('!taus_pass')
 OS = Cut('(tau1_charge * tau2_charge) == -1')
 NOT_OS = Cut('(tau1_charge * tau2_charge) != -1')
 SS = Cut('(tau1_charge * tau2_charge) == 1')
+
+P1P1 = Cut('tau1_numTrack == 1') & Cut('tau2_numTrack == 1')
+P3P3 = Cut('tau1_numTrack == 3') & Cut('tau2_numTrack == 3')
 
 P1P3 = (
     (Cut('tau1_numTrack == 1') | Cut('tau1_numTrack == 3'))
     &
     (Cut('tau2_numTrack == 1') | Cut('tau2_numTrack == 3')))
 
-P1P3_RECOUNTED = (
-    (Cut('tau1_numTrack_recounted == 1') | Cut('tau1_numTrack_recounted == 3'))
+TRACK_ISOLATION = (
+    Cut('tau1_numTrack_recounted == tau1_numTrack')
     &
-    (Cut('tau2_numTrack_recounted == 1') | Cut('tau2_numTrack_recounted == 3')))
+    Cut('tau2_numTrack_recounted == tau2_numTrack'))
 
 REGIONS = {
     'ALL': Cut(),
-    'OS': OS, # & TAUS_PASS,
-    #'OSFF': OS & TAUS_FAIL,
-    'OS_TRK': OS & P1P3_RECOUNTED, # & TAUS_PASS,
-    #'OSFF_TRK': OS & TAUS_FAIL & P1P3_RECOUNTED,
-    'nOS': NOT_OS, # & TAUS_PASS,
-    #'nOSFF': NOT_OS & TAUS_FAIL,
-    'SS': SS, #& TAUS_PASS,
-    #'SSFF': SS & TAUS_FAIL,
-    'SS_TRK': SS & P1P3_RECOUNTED, #& TAUS_PASS
-    #'SSFF_TRK': SS & TAUS_FAIL & P1P3_RECOUNTED
+    'OS': OS & TRACK_ISOLATION,
+    'OS_TRK': OS & P1P3 & TRACK_ISOLATION,
+    'nOS': NOT_OS & TRACK_ISOLATION,
+    'SS': SS & TRACK_ISOLATION,
+    'SS_TRK': SS & P1P3 & TRACK_ISOLATION,
 }
 
 TARGET_REGIONS = ['OS', 'OS_TRK']
 QCD_SHAPE_REGIONS = [
     'nOS', 'SS', 'SS_TRK',
-    #'OSFF', 'SSFF', 'SSFF_TRK'
 ]
