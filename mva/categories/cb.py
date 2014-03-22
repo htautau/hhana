eaea44ae0d7d07dff91b7b0f47888cef1237d337
Noel Dawe <Noel.Dawe@cern.ch>
2014-03-21 18:39:20 -0700

    cb: fix norm_category

--- mva/categories/cb.py
+++ mva/categories/cb.py
@@ -26,6 +26,7 @@ class Category_Cuts_VBF_LowDR(Category_Preselection):
     cuts = (CUTS_VBF_CUTBASED
             & Cut('dR_tau1_tau2 < 1.5') & Cut('resonance_pt > 140000'))
     limitbins = [0,64,80,92,104,116,132,176,INF]
+    norm_category = Category_Preselection
 
 
 class Category_Cuts_VBF_HighDR_Tight(Category_Preselection):
@@ -35,6 +36,7 @@ class Category_Cuts_VBF_HighDR_Tight(Category_Preselection):
         & (Cut('dR_tau1_tau2 > 1.5') | Cut('resonance_pt < 140000'))
         & Cut('mass_jet1_jet2 > (-250000 * dEta_jets + 1550000)'))
     limitbins = [0,64,80,92,104,116,132,152,176,INF]
+    norm_category = Category_Preselection
 
 
 class Category_Cuts_VBF_HighDR_Loose(Category_Preselection):
@@ -44,6 +46,7 @@ class Category_Cuts_VBF_HighDR_Loose(Category_Preselection):
         & (Cut('dR_tau1_tau2 > 1.5') | Cut('resonance_pt < 140000'))
         & Cut('mass_jet1_jet2 < (-250000 * dEta_jets + 1550000)'))
     limitbins = [0,64,80,92,104,116,132,152,176,INF]
+    norm_category = Category_Preselection
 
 
 class Category_Cuts_Boosted_Tight(Category_Preselection):
@@ -53,6 +56,7 @@ class Category_Cuts_Boosted_Tight(Category_Preselection):
             & (Cut('dR_tau1_tau2 < 1.5') & Cut('resonance_pt>140000')))
     #         & ((Cut('resonance_pt > (-200000 * dR_tau1_tau2 + 400000)') & Cut('resonance_pt > 140000')) | Cut('resonance_pt > 200000')))
     limitbins = [0,64,72,80,84,88,92,96,100,104,108,112,116,120,124,128,132,136,140,144,152,160,168,176,184,200,INF]
+    norm_category = Category_Preselection
 
 
 class Category_Cuts_Boosted_Loose(Category_Preselection):
@@ -63,6 +67,8 @@ class Category_Cuts_Boosted_Loose(Category_Preselection):
 #         & Cut('resonance_pt > (-200000 * dR_tau1_tau2 + 400000)') & Cut('resonance_pt < 140000')
 #         & Cut('dEta_tau1_tau2 < 1'))
     limitbins = [0,72,80,84,88,92,96,100,104,108,112,116,120,124,128,132,136,140,144,152,176,184,INF]
+    norm_category = Category_Preselection
+
 
 # --------> Added by Quentin Buat quentin(dot)buat(at)cern(dot)ch
 class Category_Cuts_VBF(Category_Preselection):
@@ -70,9 +76,12 @@ class Category_Cuts_VBF(Category_Preselection):
     label = '#tau_{had}#tau_{had} Cut-based VBF'
     cuts  = Category_Cuts_VBF_HighDR_Loose.cuts | Category_Cuts_VBF_HighDR_Tight.cuts | Category_Cuts_VBF_LowDR.cuts
     limitbins = [0,64,80,92,104,116,132,152,176,INF]
+    norm_category = Category_Preselection
+
 
 class Category_Cuts_Boosted(Category_Preselection):
     name = 'cuts_boosted'
     label = '#tau_{had}#tau_{had} Cut-based Boosted'
     cuts = Category_Cuts_Boosted_Tight.cuts | Category_Cuts_Boosted_Loose.cuts
     limitbins = [0,72,80,84,88,92,96,100,104,108,112,116,120,124,128,132,136,140,144,152,176,184,INF]
+    norm_category = Category_Preselection
