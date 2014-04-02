5cf829f85fd51a113bc8d525f31f3c3a1b319a00
qbuat <quentin.buat@cern.ch>
2014-04-02 13:29:02 -0700

    add new CR categories for CB

--- mva/categories/cb.py
+++ mva/categories/cb.py
@@ -1,7 +1,7 @@
 import math
 from rootpy.tree import Cut
 from .common import Category_Preselection
-from .selections import CUTS_VBF, CUTS_BOOSTED
+from .selections import CUTS_2J, CUTS_VBF, CUTS_BOOSTED
 
 # https://cds.cern.ch/record/1629891/files/ATL-COM-PHYS-2013-1558.pdf
 
@@ -97,13 +97,27 @@ class Category_Cuts_Boosted_Loose(Category_Preselection):
 
 
 # ------------> Categories designed for analysis control plots
+class Category_Cuts_VBF_CR(Category_Preselection):
+    name = 'cuts_vbf_cr'
+    label = '#tau_{had}#tau_{had} Cut-based VBF Control Region'
+    latex = '\\textbf{VBF Control Region}'
+    cuts  = CUTS_2J
+
+class Category_Cuts_Boosted(Category_Preselection):
+    name = 'cuts_boosted_cr'
+    label = '#tau_{had}#tau_{had} Cut-based Boosted Control Region'
+    cuts = Category_Cuts_Boosted_Tight.cuts | Category_Cuts_Boosted_Loose.cuts
+    norm_category = Category_Preselection
+
+
+
 class Category_Cuts_Boosted_Tight_NoDRCut(Category_Preselection):
     name = 'cuts_boosted_tight_nodrcut'
     label = '#tau_{had}#tau_{had} Cut-based Boosted Tight No dR Cut'
     cuts = ((- CUTS_VBF_CUTBASED) & CUTS_BOOSTED_CUTBASED
             & Cut('resonance_pt>140000') )
-    limitbins = [0,64,72,80,84,88,92,96,100,104,108,112,116,120,124,128,132,136,140,144,152,160,168,176,184,200,INF]
     norm_category = Category_Preselection
+
 
 
 
@@ -116,9 +130,3 @@ class Category_Cuts_VBF(Category_Preselection):
     norm_category = Category_Preselection
 
 
-class Category_Cuts_Boosted(Category_Preselection):
-    name = 'cuts_boosted'
-    label = '#tau_{had}#tau_{had} Cut-based Boosted'
-    cuts = Category_Cuts_Boosted_Tight.cuts | Category_Cuts_Boosted_Loose.cuts
-    limitbins = [0,72,80,84,88,92,96,100,104,108,112,116,120,124,128,132,136,140,144,152,176,184,INF]
-    norm_category = Category_Preselection
