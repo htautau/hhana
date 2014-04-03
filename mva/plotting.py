c2eb05bacbbd7d2f5c1609c2ded088478a745f35
Noel Dawe <Noel.Dawe@cern.ch>
2014-04-03 11:51:50 -0700

    updates

--- mva/plotting.py
+++ mva/plotting.py
@@ -1245,8 +1245,6 @@ def draw(name,
                 ratio_range = (0.8, 1.2)
             elif max_dev < 0.4:
                 ratio_range = (0.6, 1.4)
-            elif max_dev < 0.5:
-                ratio_range = (0.5, 1.5)
 
             ruler_high = (ratio_range[1] + 1.) / 2.
             ruler_low = (ratio_range[0] + 1.) / 2.
