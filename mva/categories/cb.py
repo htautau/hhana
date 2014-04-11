90b44f038d47059990260ba51a43a33b705a802c
qbuat <quentin.buat@cern.ch>
2014-04-11 09:35:31 -0700

    Restore the default cut-based deta_jj cut

--- mva/categories/cb.py
+++ mva/categories/cb.py
@@ -7,8 +7,7 @@ from .common import (
 # https://cds.cern.ch/record/1629891/files/ATL-COM-PHYS-2013-1558.pdf
 
 
-# DETA_JETS = Cut('dEta_jets > 2.6')
-DETA_JETS = Cut('dEta_jets > 2.0')
+DETA_JETS = Cut('dEta_jets > 2.6')
 MASS_JETS = Cut('mass_jet1_jet2 > 250000')
 
 TAU1_CENTR = Cut('tau1_centrality > %f' % (1. / math.e))
