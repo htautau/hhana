48fe912aaf14bb0a87be00bba4449176b1d18880
qbuat <quentin.buat@cern.ch>
2014-07-02 19:36:24 -0700

    Update binning

--- statstools/fixups.py
+++ statstools/fixups.py
@@ -85,14 +85,17 @@ def fix_measurement(meas,
     if merge:
         # merge bins in some channels
         process_measurement(meas,
-                            merge_bins=[(1, 2), (8, 9)],
-                            merge_bins_channels=['*cuts_vbf_highdr_tight*'])
+                            merge_bins=[(6, 7), (8, 9)],
+                            merge_bins_channels=['*cuts_vbf_highdr_loose_12*'])
         process_measurement(meas,
-                            merge_bins=[(1, 2)],
-                            merge_bins_channels=['*cuts_vbf_highdr_11*'])
+                            merge_bins=[(2, 3), (4, 5), (6, 7)],
+                            merge_bins_channels=['*cuts_vbf_highdr_tight_12*'])
         process_measurement(meas,
-                            merge_bins=[(2, 3), (4, 5), (6, 7), (8, 9), (10, 11), (12, 13), (14, 16), (18, 19)],
-                            merge_bins_channels=['*cuts_boosted_loose_11*'])
+                            merge_bins=[(3, 4), (5, 6), (7, 8), (9, 10), (11, 12), (13, 14), (15, 16), (17, 18)],
+                            merge_bins_channels=['*cuts_boosted_tight_12*'])
+        process_measurement(meas,
+                            merge_bins=[(3, 4), (5, 6), (7, 8), (9, 10), (11, 12), (13, 14), (15, 16), (17, 18), (19, 20)],
+                            merge_bins_channels=['*cuts_boosted_loose_12*'])
 
     # fill empty bins with the average sample weight
     # the so-called "Kyle-fix"
