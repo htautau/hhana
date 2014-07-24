d06e66a0045617d15f27c07ba2bb2f2e3f9f5697
Noel Dawe <Noel.Dawe@cern.ch>
2014-07-24 01:36:55 -0700

    init updates

--- statstools/extern/__init__.py
+++ statstools/extern/__init__.py
@@ -1,17 +1,9 @@
 import rootpy.compiled as C
 import os
 HERE = os.path.dirname(os.path.abspath(__file__))
-if os.getenv('OLD', False):
-    C.register_file(os.path.join(HERE, 'old_runSig.C'),
-                    ['significance'])
-    def make_asimov_data(*args, **kwargs):
-        return None
-    from rootpy.compiled import significance
-else:
-    C.register_file(os.path.join(HERE, 'runSig.C'),
-                    ['significance', 'make_asimov_data'])
-    from rootpy.compiled import significance, make_asimov_data
-
+C.register_file(os.path.join(HERE, 'runSig.C'),
+                ['significance', 'make_asimov_data'])
+from rootpy.compiled import significance, make_asimov_data
 C.register_file(os.path.join(HERE, 'AsymptoticsCLs.C'),
                 ['AsymptoticsCLs'])
 from rootpy.compiled import AsymptoticsCLs
