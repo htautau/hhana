2096cd69a65d11f57d4f6d01348c785a5f343873
Noel Dawe <Noel.Dawe@cern.ch>
2014-08-04 19:11:03 -0700

    missing import

--- mva/plotting/__init__.py
+++ mva/plotting/__init__.py
@@ -4,3 +4,4 @@ from .draw import draw
 from .histfactory import draw_channel_array, draw_channel
 from .classify import plot_clf, plot_grid_scores, hist_scores
 from .compare import draw_ratio
+from .contours import draw_contours
